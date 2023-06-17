2a6f9adc213da755efda9519e4906b343808cc78
BOB <vasiasko112@gmail.com>
2023-06-18 00:00:54 +0400

    build

--- src/PiniaStore/piniadb.ts
+++ src/PiniaStore/piniadb.ts
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 
 export const Store = defineStore('DefaultStore', () => {
   return {
-    dbCountry: [] as Array<any> || null,
+    dbCountry: ([] as Array<any>) || null,
     acceptGPS: false as any,
     currentCountry: {
       name: '',
