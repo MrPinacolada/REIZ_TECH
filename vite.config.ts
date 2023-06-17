d7b372fb6cbd38ec756eafbca0755ed66aeaf9e0
BOB <vasiasko112@gmail.com>
2023-06-18 00:05:00 +0400

    none

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 export default defineConfig({
-  base:'/REIZ_TECH'
+  base:'/REIZ_TECH',
   plugins: [
     vue(),
   ],
